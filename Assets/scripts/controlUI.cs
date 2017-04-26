3cf70dbfb1232595b7d814163ac2ace81eb16635
Daniel Anguiano <daniel121389@gmail.com>
2017-04-26 17:33:09 -0500

    Juego
    Se agregaron todos los archivos de unity que ya teniamos creados

---
+++ Assets/scripts/controlUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class controlUI : MonoBehaviour {
+
+	public GameObject panelInicio;
+	public GameObject panelPausa;
+	public GameObject panelSeleccion;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void IniciarJuego() {
+		panelInicio.SetActive (false);
+	}
+		
+
+}
